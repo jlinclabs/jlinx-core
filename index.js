907a7345b527a017bd62b3f5c8ad51433225c9d6
Jared Grippe <jared@deadlyicon.com>
2022-07-08 11:51:11 -0700

    WIP

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
-const fs = require('fs/promises')
 const crypto = require('hypercore-crypto')
 const b64 = require('urlsafe-base64')
 const b4a = require('b4a')
 const sodium = require('sodium-universal')
-const base58BitcoinAlphabet = '123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz';
-const base58 = require('base-x')(base58BitcoinAlphabet);
+const base58 = require('./base58')
 
+exports.base58 = base58
 exports.sign = crypto.sign
 exports.verify = crypto.verify
 exports.randomBytes = crypto.randomBytes
@@ -52,54 +51,4 @@ exports.validateEncryptingKeyPair = function (keyPair) {
   const plain = b4a.allocUnsafe(cipher.length)
   sodium.crypto_box_open_detached(plain, cipher, mac, nonce, keyPair.publicKey, keyPair.secretKey)
   return b4a.equals(plain, message)
-}
-
-exports.fsExists = async function (path) {
-  try {
-    await fs.stat(path)
-    return true
-  } catch (error) {
-    if (error.code === 'ENOENT') return false
-    throw error
-  }
-}
-
-exports.signingKeyToDidDocument = function(publicKey){
-  const base68EncodedPK = base58.encode(exports.keyToBuffer(publicKey))
-  const publicKeyMultibase = `z6mk${base68EncodedPK}`
-  const did = `did:key:${publicKeyMultibase}`
-  const didDocument = {
-    "@context": [
-      "https://www.w3.org/ns/did/v1",
-      "https://w3id.org/security/suites/ed25519-2020/v1",
-      "https://w3id.org/security/suites/x25519-2020/v1"
-    ],
-    "id": `${did}`,
-    "verificationMethod": [{
-      "id": `${did}#${publicKeyMultibase}`,
-      "type": "Ed25519VerificationKey2020",
-      "controller": `${did}`,
-      "publicKeyMultibase": `${publicKeyMultibase}`
-    }],
-    "authentication": [
-      `${did}#${publicKeyMultibase}`
-    ],
-    "assertionMethod": [
-      `${did}#${publicKeyMultibase}`
-    ],
-    "capabilityDelegation": [
-      `${did}#${publicKeyMultibase}`
-    ],
-    "capabilityInvocation": [
-      `${did}#${publicKeyMultibase}`
-    ],
-    "keyAgreement": [{
-      "id": `${did}#${publicKeyMultibase}`,
-      "type": "X25519KeyAgreementKey2020",
-      "controller": `${did}`,
-      "publicKeyMultibase": `${publicKeyMultibase}`
-    }]
-  }
-
-  return didDocument
 }
